cd53bf8509067c78a3e0d6cfa433ff2e56badd41
Rudy Cazabon <rudycazabon@gmail.com>
2020-04-26 12:04:48 -0700

    First commit.

---
+++ assets/js/ipc.js
@@ -0,0 +1,39 @@
+const {ipcRenderer} = require('electron')
+
+window.ipc = window.ipc || {},
+function(n) {
+    ipc.messaging = {
+
+      sendOpenSecondWindowEvent: function() {
+        ipcRenderer.send('open-second-window', 'an-argument')
+      },
+
+      sendXeyesEvent: function() {
+        ipcRenderer.send('open-xeyes', 'an-argument')
+      },
+
+      sendCloseSecondWindowEvent: function() {
+        ipcRenderer.send('close-second-window', 'an-argument')
+      },
+
+      init: function() {
+        $('#open-secondwindow-button').click( function () {
+          ipc.messaging.sendOpenSecondWindowEvent()
+        })
+
+        $('#open-xeyes-button').click( function () {
+          ipc.messaging.sendXeyesEvent()
+        })
+
+        $('#close-me-button').click( function () {
+          ipc.messaging.sendCloseSecondWindowEvent()
+        })
+
+      }
+    };
+
+    n(function() {
+        ipc.messaging.init();
+    })
+
+}(jQuery);
